786d33de1920c8e4a2765b608852edd14a68d240
Irnehs <henritschulz@gmail.com>
2023-08-21 12:11:47 -0700

    Added hardware outputs and Serial inits

--- Simulator/Simulator.ino
+++ Simulator/Simulator.ino
@@ -1,7 +1,31 @@
+/** Simulator.ino
+  * Last modified 8/21/23
+  * 
+  *
+
+**/
+
+#define IR1 13
+#define IR2 12
+#define IR3 11
+
+#define Computer Serial
+#define RFID Serial1
+
+
+
 void setup() {
+  Computer.begin(9600);
+  RFID.begin(9600);
+
+  pinMode(IR1, OUTPUT);
+  pinMode(IR2, OUTPUT);
+  pinMode(IR3, OUTPUT);
 
 }
 
+
 void loop() {
+
     
 }
